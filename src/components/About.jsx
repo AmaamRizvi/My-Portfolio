586753945d3b21dc4b38168712efe3369dfe3638
Amaam_Rizvi <amaamrizvi6557@gmail.com>
2024-07-04 22:02:06 +0530

    new updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
           transition={{ duration: 0.5 }}
           className="w-full lg:w-1/2 lg:p-8"
         >
-          <div className="flex items-center justify-center w-[65vh] ml-9">
+          <div className="flex items-center justify-center">
             <img className="rounded-2xl" src={aboutImg} alt="" />
           </div>
         </motion.div>
